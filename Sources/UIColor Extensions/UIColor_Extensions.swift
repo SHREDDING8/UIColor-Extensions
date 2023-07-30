7e6880913387212bbc805edf3eda5788769c2ecb
Egor Zavrazhnov <zavrazhnov.zhora@mail.ru>
2023-07-30 22:32:49 +0300

    added hex

--- Sources/UIColor Extensions/UIColor_Extensions.swift
+++ Sources/UIColor Extensions/UIColor_Extensions.swift
@@ -1,2 +1,44 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import UIKit
+// MARK: - HEX COLOR
+extension UIColor{
+    func toHex() -> String {
+        let components = self.cgColor.components
+        let r: CGFloat = components?[0] ?? 0.0
+        let g: CGFloat = components?[1] ?? 0.0
+        let b: CGFloat = components?[2] ?? 0.0
+
+        let hexString = String.init(format: "#%02lX%02lX%02lX", lroundf(Float(r * 255)), lroundf(Float(g * 255)), lroundf(Float(b * 255)))
+        
+        return hexString
+     }
+    
+    convenience init(hexString:String) {
+        var colorString = hexString.trimmingCharacters(in: .whitespacesAndNewlines)
+        colorString = colorString.replacingOccurrences(of: "#", with: "").uppercased()
+
+        let alpha: CGFloat = 1.0
+        let red: CGFloat = Self.colorComponentFrom(colorString: colorString, start: 0, length: 2)
+        let green: CGFloat = Self.colorComponentFrom(colorString: colorString, start: 2, length: 2)
+        let blue: CGFloat = Self.colorComponentFrom(colorString: colorString, start: 4, length: 2)
+        
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+    
+    static private func colorComponentFrom(colorString: String, start: Int, length: Int) -> CGFloat {
+        
+        let startIndex = colorString.index(colorString.startIndex, offsetBy: start)
+        let endIndex = colorString.index(startIndex, offsetBy: length)
+        let subString = colorString[startIndex..<endIndex]
+        let fullHexString = length == 2 ? subString : "\(subString)\(subString)"
+        var hexComponent: UInt32 = 0
+
+        guard Scanner(string: String(fullHexString)).scanHexInt32(&hexComponent) else {
+            return 0
+        }
+        let hexFloat: CGFloat = CGFloat(hexComponent)
+        let floatValue: CGFloat = CGFloat(hexFloat / 255.0)
+        return floatValue
+    }
+}
