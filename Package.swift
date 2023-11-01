17837d7d884393be0556c546a7a32e33a8af02f5
Egor Zavrazhnov <88378430+SHREDDING8@users.noreply.github.com>
2023-11-01 13:33:14 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "UIColorExtensions",
-            targets: ["UIColorExtensions"]),
+            name: "UIColor Extensions",
+            targets: ["UIColor Extensions"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
